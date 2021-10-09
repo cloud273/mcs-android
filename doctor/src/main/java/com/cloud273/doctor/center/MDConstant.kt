d38b594c35e54ccd42822945e0ab92acb5996eb9
dungnguyen2703 <dungnguyen2703@live.com>
2021-10-09 15:40:19 +0700

    update termurl

--- doctor/src/main/java/com/cloud273/doctor/center/MDConstant.kt
+++ doctor/src/main/java/com/cloud273/doctor/center/MDConstant.kt
@@ -4,5 +4,5 @@ import com.cloud273.backend.hostUrl
 
 const val aboutUsUrl = "$hostUrl/support/doctor/about.html"
 const val faqUrl = "$hostUrl/support/doctor/faq.html"
-const val termUrl = "$hostUrl/support/doctor/termcondition.html"
+const val termUrl = "$hostUrl/support/doctor/term_condition.html"
 const val contactUrl = "$hostUrl/support/doctor/contact.html"
--- patient/src/main/java/com/cloud273/patient/center/MPConstant.kt
+++ patient/src/main/java/com/cloud273/patient/center/MPConstant.kt
@@ -4,5 +4,5 @@ import com.cloud273.backend.hostUrl
 
 const val aboutUsUrl = "$hostUrl/support/patient/about.html"
 const val faqUrl = "$hostUrl/support/patient/faq.html"
-const val termUrl = "$hostUrl/support/patient/termcondition.html"
+const val termUrl = "$hostUrl/support/patient/term_condition.html"
 const val contactUrl = "$hostUrl/support/patient/contact.html"
